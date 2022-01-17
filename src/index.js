2362116a79b4d4b75b8b7a0b3c7b0b88a4e5d26a
igordbauer <igordbauer@gmail.com>
2022-01-16 21:34:13 -0300

    challenge concluido

--- src/index.js
+++ src/index.js
@@ -1,28 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core';
+import { Box } from '@material-ui/system';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { createTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import Login from 'views/Login';
+import bgImage from 'assets/background.png';
 
-const Theme = responsiveFontSizes(createTheme({
-  palette: {
-    primary: {
-      main: "#3E83FF"
+
+const Theme = responsiveFontSizes(
+  createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1536,
+      },
     },
-    secondary: {
-      main: "#28DC8E"
-    }
-  },
-  shape: {
-    borderRadius: 8
-  }
-}))
+
+    palette: {
+      primary: {
+        main: '#3E83FF',
+      },
+      background: {
+        paper: 'rgb(255,255,255,0.7)',
+        default: 'white',
+      },
+      secondary: {
+        main: '#28DC8E',
+      },
+    },
+    shape: {
+      borderRadius: 8,
+    },
+  })
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
-      <Login />
+      <Box sx={{
+        display: 'flex',
+        alignItems: 'center',
+        backgroundImage: `url(${bgImage})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        minHeight: '100vh',
+        minWidth: '100%',
+      }}>
+        <Login />
+      </Box>
     </ThemeProvider>
+    <CssBaseline />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,10 +1,129 @@
-import React from "react";
-// import { Formik, Field, Form } from "formik";
-// import * as Yup from "yup";
-// import { TextField } from "@material-ui/core";
+import {
+  Container,
+  Typography,
+  TextField,
+  Box,
+  Grid,
+  Card,
+  Button,
+  CardMedia,
+} from '@material-ui/core';
+import logoChimu from 'assets/logo_chimu.png';
+import React, { useState } from 'react';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = () => {
-    return <div/>
-}
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+
+  const schema = Yup.object().shape({
+    email: Yup.string().email('Invalid Email').required('Required!'),
+    password: Yup.string()
+      .min(4, 'Your password is too short!')
+      .max(9, 'Your password is too long!')
+      .required('Required!'),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      email: enteredEmail,
+      password: enteredPassword,
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      setEnteredEmail(values.email);
+      setEnteredPassword(values.password);
+      console.log('Email:' + values.email + ', Password:' + values.password);
+    },
+  });
+
+  return (
+    <Container
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      <Card
+        sx={{
+          maxWidth: '474px',
+        }}
+      >
+        <form onSubmit={formik.handleSubmit}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Box my={6}>
+              <CardMedia
+                component="img"
+                height="80px"
+                width="auto"
+                image={logoChimu}
+                alt="Chimu"
+              />
+            </Box>
+            <Grid
+              container
+              my={4}
+              px={3}
+              py={4}
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              <Grid item md={12} xs={12}>
+                <Typography>Username or E-mail</Typography>
+                <TextField
+                  fullWidth
+                  id="email"
+                  name="email"
+                  placeholder="nome@email.com"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  helperText={formik.touched.email && formik.errors.email}
+                />
+              </Grid>
+              <Grid item md={12} xs={12}>
+                <Typography>Password</Typography>
+                <TextField
+                  fullWidth
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="topsecret"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.password && Boolean(formik.errors.password)
+                  }
+                  helperText={formik.touched.password && formik.errors.password}
+                />
+              </Grid>
+            </Grid>
+
+            <Button
+              variant="contained"
+              type="submit"
+              size="large"
+              sx={{
+                my: 4,
+              }}
+            >
+              Log In
+            </Button>
+          </Box>
+        </form>
+      </Card>
+    </Container>
+  );
+};
 
 export default Login;
